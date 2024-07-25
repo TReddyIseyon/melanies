77e57111ac4e2c02c53849d9f74482ec28b09a81
TReddyIseyon <Tarun.Reddy@iseyon.com>
2024-07-25 16:28:07 +0530

    Create streamit_app.py
    copied from custom smoothies CIS app

---
+++ streamit_app.py
@@ -0,0 +1,56 @@
+# Import python packages
+
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+
+# Write directly to the app
+st.title(":cup_with_straw: Customize Your Smoothie:cup_with_straw:")
+st.write(
+    """Choose the fruits you want in your custom smoothie !
+    """
+)
+name_on_order=st.text_input("Name on the smoothie:")
+st.write('The name on your smoothie will be:',name_on_order)
+session = get_active_session()
+my_dataframe=session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
+#
+
+
+ingredients_list = st.multiselect(
+    "Choose upto five ingridients:",
+    my_dataframe , max_selections=5 )
+if ingredients_list:
+  
+  ingredients_string=''  
+  for fruit_chosen in ingredients_list:
+      ingredients_string+=fruit_chosen + ' '
+  st.write(ingredients_string)    
+  my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+  
+  #st.write(my_insert_stmt)
+  if ingredients_string:
+      
+       session.sql(my_insert_stmt).collect()
+       st.success('Your Smoothie is ordered!', icon="✅")
+
+
+
+my_dataframe2 = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
+editable_df = st.data_editor(my_dataframe2)
+submitted=st.button('Submit Order')
+
+if submitted:
+    
+    og_dataset = session.table("smoothies.public.orders")
+    edited_dataset = session.create_dataframe(editable_df)
+    try:
+         og_dataset.merge(edited_dataset
+                     , (og_dataset['order_uid'] == edited_dataset['order_uid'])
+                     , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                    )
+         st.success('Your Smoothie is ordered!', icon="✅")
+    except:
+        st.write("Something went Wrong")
